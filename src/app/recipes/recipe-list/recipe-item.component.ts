d7d6f18a32f5ab76f95beecdde34769b54e046ec
Gene Higgins <ehigginsiii@cnm.edu>
2016-12-02 17:30:02 -0700

    added routes and child routes

--- src/app/recipes/recipe-list/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Recipe} from "../recipe";
 
 @Component({
@@ -8,13 +8,9 @@ import {Recipe} from "../recipe";
     '.img-thumbnail{ max-width: 120px;}'
   ]
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent {
   @Input() recipe: Recipe;
-  recipeId: number;
+  @Input() recipeId: number;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
