e6aa807e4a540bdd9be24315f8ef04d8a13ff509
Gene Higgins <ehigginsiii@cnm.edu>
2016-12-16 09:58:40 -0700

    moved recipes to its own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,37 +5,27 @@ import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header.component';
-import {RecipesComponent} from './recipes/recipes.component';
-import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
-import {RecipeItemComponent} from './recipes/recipe-list/recipe-item.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {RecipeStartComponent} from './recipes/recipe-start.component';
 import {DropdownDirective} from './dropdown.directive';
 import {RecipeService} from "./recipes/recipe.service";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {routing} from "./app.routing";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {RecipesModule} from "./recipes/recipes.module";
 
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeItemComponent,
-        RecipeDetailComponent,
         DropdownDirective,
-        RecipeEditComponent,
-        RecipeStartComponent
     ],
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         HttpModule,
         routing,
-        ShoppingListModule
+        ShoppingListModule,
+        RecipesModule
     ],
     providers: [
         RecipeService,
